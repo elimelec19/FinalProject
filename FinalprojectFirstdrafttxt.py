299aca93f4ff516ddb8adc0e5f8e7a7e096456d9
elimelec19 <82791095+elimelec19@users.noreply.github.com>
2021-04-27 09:24:39 -0400

    Add files via upload

---
+++ FinalprojectFirstdrafttxt.py
@@ -0,0 +1,57 @@
+# Final project 
+#Learning Math for kids, user creation !
+
+
+
+# Function that open the file
+def openfile (open, str)
+    with open(User) as a:
+        linea =a.readline()
+        linea=linea.strip
+return()
+
+print ("Welcome to The funny place for kids to learn math")
+option = input ("Do you have an account with us press 1 for yes and 2 for no?")
+try:
+    foption=float(option)
+except:
+    print("try numeric numbers")
+    quit()
+if (option == '1')
+    print("type your user:")
+    usern=string()
+    print ("what do you want to learn today \n To add press '+' \n To Substract Press '-' \n To Multiply press '*' \n To Divide press '/' " )
+    math=input()
+
+    try:
+        fmath=float(math)
+    except:
+        print("try +, -, * or /")
+        quit()
+ if (math == '+')
+ import random
+ while(tryiescounter=3)
+ for _ in range(1):
+     val1=random.randint(1,11)
+     print("Num 1:",val1)
+ for _ in range(1):
+     val2=random.randint(1,11)
+     print("Num 2:",val2)
+
+print(" Adding ",val1, "+",val2," = :")
+answer1=input()
+sum= val1+val2
+if  (answer1 = sum )
+  print ("correct")
+else 
+  print ("incorrect")
+tryiescounter=tryiescounter+1
+
+
+if (option == '2')
+    with open('User.txt', 'w') as f:
+    nameuser=input("What is your name :")
+    password=input("Create password")
+    while(retypepas!=pastword)
+    retypepass=input("retype the password")
+    userpassword.append(time)
